598c32f26b0372d47dad94ab5ba4488d79b47601
maheshp345 <pmaheshcareerpedia@gmail.com>
2023-12-13 22:28:40 +0530

    first push

---
+++ app.js
@@ -0,0 +1,69 @@
+
+let container1 = document.querySelector(".container1")
+let image = document.querySelector(".image")
+let bigImage1 = document.querySelector(".big-image1");
+let bigImage2 = document.querySelector(".big-image2");
+let bigImage3 = document.querySelector(".big-image3");
+let bigImage4 = document.querySelector(".big-image4");
+let bigImage5 = document.querySelector(".big-image5");
+let bigImage6 = document.querySelector(".big-image6");
+let bigImage7 = document.querySelector(".big-image7");
+let bigImage8 = document.querySelector(".big-image8");
+
+function enlargeImg1() {
+    bigImage1.style.display = "block";
+    container1.style.opacity = "0.1";
+}
+
+function enlargeImg2() {
+    bigImage2.style.display = "block";
+    container1.style.opacity = "0.1";
+}
+
+function enlargeImg3() {
+    bigImage3.style.display = "block";
+    container1.style.opacity = "0.1";
+}
+
+function enlargeImg4() {
+    bigImage4.style.display = "block";
+    container1.style.opacity = "0.1";
+}
+
+function enlargeImg5() {
+    bigImage5.style.display = "block";
+    container1.style.opacity = "0.1";
+}
+
+function enlargeImg6() {
+    bigImage6.style.display = "block";
+    container1.style.opacity = "0.1";
+}
+
+function enlargeImg7() {
+    bigImage7.style.display = "block";
+    con
+    tainer1.style.opacity = "0.1";
+}
+
+function enlargeImg8() {
+    bigImage8.style.display = "block";
+    container1.style.opacity = "0.1";
+}
+
+
+
+let cross = document.querySelector(".cross")
+
+function displayNone() {
+    bigImage1.style.display = "none";
+    bigImage2.style.display = "none";
+    bigImage3.style.display = "none";
+    bigImage4.style.display = "none";
+    bigImage5.style.display = "none";
+    bigImage6.style.display = "none";
+    bigImage7.style.display = "none";
+    bigImage8.style.display = "none";
+    container1.style.opacity = "1";
+
+}
